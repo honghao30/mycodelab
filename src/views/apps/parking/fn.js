b3875ed9e885a733c73a4382ffa49efc76af1729
hongsh <hongsh@the-51.com>
2024-01-05 17:25:17 +0900

    주차장

--- src/views/apps/parking/fn.js
+++ src/views/apps/parking/fn.js
@@ -14,5 +14,8 @@ export function getRandomIndex(array) {
 //차량번호 체크
 export function checkCarNum(value) {
     const regex = /\d{2,3}[가-힣]{1}\d{4}/gm;
-    regex.test(value)
+    if(regex.test(value)) {
+        return true
+    }
+    return false
 }
